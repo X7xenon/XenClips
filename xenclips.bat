2d240d6aeaf136b88b4e1f1070b43f67f5a4cebc
Xenon <xenon@example.com>
2026-08-10 10:27:57 +0530

    fix: make launcher scripts dynamically resolve directory path

--- xenclips.bat
+++ xenclips.bat
@@ -1,7 +1,7 @@
 @echo off
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-start /min "Xenclips Backend" cmd /c "cd /d X:\Millionaire\Xenclips_v0.7 && uvicorn server:app --reload --host 0.0.0.0 --port 8000"
-start /min "Xenclips Frontend" cmd /c "cd /d X:\Millionaire\Xenclips_v0.7\xenclips && npm run dev -- --host"
+start /min "Xenclips Backend" cmd /c "cd /d "%~dp0" && uvicorn server:app --reload --host 0.0.0.0 --port 8000"
+start /min "Xenclips Frontend" cmd /c "cd /d "%~dp0xenclips" && npm run dev -- --host"
 timeout /t 3 /nobreak > nul
 start http://localhost:8080
 exit
